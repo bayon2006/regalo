c3a9711fa63e06062048b38e7be09abeca9a36fa
bayon2006 <alejandrobayonmon2006@gmail.com>
2026-06-17 01:52:29 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const slides=document.querySelectorAll('.slide');
+let current=0;
+
+function nextSlide(){
+ if(current<slides.length-1){
+   slides[current].classList.remove('active');
+   current++;
+   slides[current].classList.add('active');
+ }
+}
+window.nextSlide=nextSlide;
+
+const hearts=document.getElementById('hearts');
+for(let i=0;i<35;i++){
+ const h=document.createElement('div');
+ h.className='heart';
+ h.innerHTML=['💙','💙','💙','💙'][Math.floor(Math.random()*4)];
+ h.style.left=Math.random()*100+'vw';
+ h.style.animationDuration=(6+Math.random()*8)+'s';
+ h.style.animationDelay=(Math.random()*5)+'s';
+ hearts.appendChild(h);
+}
+
+
